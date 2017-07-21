2580f88ee0a6974f9019c379f8674ab1731fd461
Paige <plapaige@amazon.com>
2017-07-21 10:07:04 -0700

    hi

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 var Alexa = require('alexa-sdk');
+const APP_ID = 'amzn1.ask.skill.8176348c-42de-4356-b821-7d79f6039af6';
+
+var states = {
+    MAINMENU: '_MAINMENU', 
+};
 
 exports.handler = function(event, context, callback){
-    var alexa = Alexa.handler(event, context);
+    var alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(newSessionHandlers, mainMenuHandlers);
+    alexa.execute();
+};
+
+var newSessionHandlers = {
+    'NewSession': function () {
+        this.handler.state = states.MAINMENU;
+        this.emit(':ask', strings.WELCOME_TEXT);
+    },
+
+    'SessionEndedRequest': function () {
+        this.emit(':tell', strings.GOODBYE);
+    },
+
+    'Unhandled': function() {
+        this.emit('NewSession');
+    }
+};
+
+var mainMenuHandlers = Alexa.CreateStateHandler(states.MAINMENU, {
+    'CheckIsExpired': function() {
+        this.emit(':tell', "Yes it's expired");
+    },
+
+    'Unhandled': function() {
+        this.emit(':tell', "I don't know");
+    }
+});
+
+var strings = {
+    // Welcome Strings
+    WELCOME_TEXT: 'Ask me if something is expired',
+    GOODBYE: 'Goodbye!'
 };
